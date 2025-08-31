665bb7e886afee1e1d911162802370ee21f18ea3
Alex <connectionwithme1@gmail.com>
2025-08-31 08:19:09 +0300

    task 1

---
+++ src/database/data_base-1.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    ['id' => 1, 'name' => ' ivan ', 'email' => ' IVAN@EXAMPLE.COM '],
+    ['id' => 2, 'name' => '  OLGA', 'email' => 'olga@example.com '],
+    ['id' => 3, 'name' => 'petr ', 'email' => '  PETR@example.com'],
+];
+
